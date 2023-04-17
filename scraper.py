5016a96dd93fc24da2070b984ac31e8271ca6578
Luc Swinkels <luc.swinkels@live.nl>
2023-04-17 12:06:26 +0200

    test

--- scraper.py
+++ scraper.py
@@ -3,15 +3,15 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 # URL of the Amazon page to scrape
-url = "https://www.amazon.com/dp/B00X4WHP5E"
+url = "https://www.amazon.com/Eargasm-Musicians-Motorcycles-Sensitivity-Conditions/dp/B019M576XW/ref=sr_1_9?crid=3TJTCL31OEHGC&keywords=Motorcycle%2BEar%2BPlugs&qid=1681721023&sprefix=motorcycle%2Bear%2Bplugs%2Caps%2C173&sr=8-9&th=1"
 
 # Make a GET request to the URL and get the HTML content
 response = requests.get(url)
 content = response.content
-
+print(content)
 # Parse the HTML content using BeautifulSoup
 soup = BeautifulSoup(content, 'html.parser')
-
+# print(soup.prettify())
 # Find all the product containers on the page
 product_containers = soup.find_all('div', {'class': 's-result-item'})
 
@@ -43,6 +43,9 @@ for container in product_containers[:40]:
 
 # Create a Pandas dataframe from the product details list
 df = pd.DataFrame(product_details)
+
+# Print the dataframe to the console
+print(df.to_string(index=False))
 
 # Export the dataframe to an Excel file
 df.to_excel('product_details.xlsx', index=False)
